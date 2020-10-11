1c289ff627c631151d97367a81c71399406fe007
Usman Awan <65129359+FeezGhost@users.noreply.github.com>
2020-10-11 15:28:35 +0500

    Update settings.py

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -129,6 +129,6 @@ STATICFILES_DIRS = [
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'feezoocrazy420@gmail.com'
-EMAIL_HOST_PASSWORD = 'crazy420'
+EMAIL_HOST_PASSWORD = 'wssqktjsldfugjsi'
 EMAIL_PORT = 587
 # EMAIL_USE_SLS = False
